6885c59b01eb4b9fdf5cd79e51ffa7980cc1b13e
payalpatra <payalpatra105@gmail.com>
2021-08-18 21:40:15 +0530

    Set Matrix & Pascal Triangle'

---
+++ Arrays/7_SetMatrixZeros.js
@@ -0,0 +1,72 @@
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+
+// You must do it in place.
+
+// Example 1: 
+
+// Input: matrix = [[1,1,1],
+//                  [1,0,1],
+//                  [1,1,1]]
+
+// Output:           [[1,0,1],
+//                   [0,0,0],
+//                   [1,0,1]]
+
+// Example 2: 
+
+// Input: matrix = [[0, 1, 2, 0],
+//                  [3, 4, 5, 2],
+//                  [1, 3, 1, 5]
+
+// Output:           [ [ 0, 0, 0, 0 ], 
+//                     [ 0, 4, 5, 0 ], 
+//                     [ 0, 3, 1, 0 ]
+//                   ]
+
+
+
+function setMatrixZeros(matrix) {
+
+    const dimensions = [matrix.length, matrix[0].length];
+
+    let [m, n] = dimensions
+
+    let Row = new Array(m)
+    let Col = new Array(n)
+
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++) {
+            if (matrix[row][col] === 0) {
+                // console.log(row, col)
+                Row[row] = 0;
+                Col[col] = 0;
+
+            }
+
+        }
+    }
+
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[0].length; col++) {
+            if (Row[row] === 0 || Col[col] === 0) {
+                matrix[row][col] = 0;
+            }
+
+        }
+    }
+    return matrix;
+
+}
+
+console.log(setMatrixZeros([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]))
+
+// OUTPUT
+
+/*
+
+TIME COMPLEXITY - O(M * N) + 0(M * N) = 2(M * N)
+SPACE COMPLEXITY - O(N) + 0(M)
+
+*/
---
+++ Arrays/8_PascalTriangle.js
@@ -0,0 +1,83 @@
+// Given an integer numRows, return the first numRows of Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: numRows = 5
+// Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+// How To Find the value of an element at any given row and Column
+
+// Formula - R-1 
+//               C 
+//                 c-1
+
+// How To Compute Combination Values
+
+// 4 C 2 = 4 * 3 / 2 * 1
+// nCr = n! / r!( n -r)!
+
+// Source Code To Compute Combination Values
+
+
+// How To Compute Permutation Values
+
+// 4 P 2 = 4 * 3 * 2 / 2 * 1
+// nPr = n! / ( n -r)!
+
+// Source Code To Compute Combination Values
+
+function Combination(n, r) {
+    let res = 1;
+    for (let i = 0; i < r; ++i) {
+        res = res * (n - i);
+        res = res / (i + 1)
+    }
+    return res
+}
+
+console.log(Combination(4, 3))
+
+function fact(n){
+    if (n === 0|| n === 1) return 1
+    return n * fact(n - 1)
+}
+
+function Permutation(n, r) {
+    let P = 1;
+
+    // Compute n*(n-1)*(n-2)....(n-k+1)
+    for (let i = 0; i < r; i++)
+        P = fact(n) / fact(r)
+
+
+    return P;
+}
+
+console.log(Permutation(4, 3))
+
+var generate = function (numRows) {
+
+    // Initialize an Pascal with the given input length
+    let pascal = new Array(numRows.length)
+
+    // Initiallize first and last element of the row with 1
+    for (let i = 0; i < numRows; i++) {
+
+        pascal[i] = new Array(i + 1)
+
+        // First element and last element initialized to 1
+        pascal[i][0] = pascal[i][i] = 1;
+
+        // Traverse till no of rows 
+        // No elements = no of rows
+        for (let j = 1; j < i; j++) {
+            pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        }
+
+    }
+    return pascal
+
+};
+
+console.log(generate(5))
