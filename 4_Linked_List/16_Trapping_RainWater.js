61ae3216cc67aa34351d6ad0fc06ba000bf20e75
payalpatra <payalpatra105@gmail.com>
2021-10-23 01:51:28 +0530

    Trapping Water

--- 4_Linked_List/16_Trapping_RainWater.js
+++ 4_Linked_List/16_Trapping_RainWater.js
@@ -10,57 +10,70 @@ var trap = function (height) {
         let max = height[index];
         for (let i = index; i >= 0; i--) {
             if (height[i - 1] > max) {
-                max = height[i - 1]
+                max = height[i - 1];
             }
         }
 
-        return max
+        return max;
     }
 
     function right(index) {
         let max = height[index];
         for (let i = index; i < height.length - 1; i++) {
             if (height[i + 1] > max) {
-                max = height[i + 1]
+                max = height[i + 1];
             }
         }
 
-        return max
+        return max;
     }
 
     let water = 0;
     for (let i = 0; i < height.length - 1; i++) {
-        let leftM = left(i)
-        let rightM = right(i)
-        let curr = Math.min(leftM, rightM) - height[i]
+        let leftM = left(i);
+        let rightM = right(i);
+        let curr = Math.min(leftM, rightM) - height[i];
 
-        water = water + curr
+        water = water + curr;
     }
-    return water
+    return water;
 };
-
 
 // Optimized Solution
 // Two Pointer
 
 var trap2 = function (height) {
-    // Total Water 
+    // Total Water
     let water = 0;
 
     let left = 0;
     let right = height.length - 1;
 
     let leftM = 0;
-    let RightM = 0;
+    let rightM = 0;
 
+    while (left <= right) {
+        if (height[left] >= leftM) {
+            leftM = height[left];
+        }
 
+        if (height[right] >= rightM) {
+            rightM = height[right];
+        }
 
-    return water
+        if (leftM > rightM) {
+            water = water + rightM - height[right];
+            right--;
+        }else{
+            water = water + leftM - height[left];
+            left++;
+        }
+    }
 
+    return water;
 };
 
-
-console.log(trap2())
+console.log(trap2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
 
 /*
 
